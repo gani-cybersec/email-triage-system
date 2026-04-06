857d458b0d937457d8c6621032b29ad8ff5c8b63
gani-cybersec <128181057@sastra.ac.in>
2026-04-07 00:16:31 +0530

    Delete my_env/env directory

--- my_env/env/graders.py
+++
@@ -1,7 +0,0 @@
-def compute_reward(action, correct_label):
-    if action == correct_label:
-        return 1   # correct prediction
-    elif action is None:
-        return 0   # no action / invalid
-    else:
-        return -1  # wrong prediction
