3a2d97e7e8dd0980ae6a38c369ff08a57ae7b1f0
gani-cybersec <128181057@sastra.ac.in>
2026-04-09 18:19:07 +0530

    Delete server/__init__.py

--- server/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Server package initialization.
-This file ensures the server directory is treated as a Python package.
-No heavy imports should be placed here to avoid circular dependencies.
-"""
