e9397eaadeec9929640d826acaa15a7daf556c0d
gani-cybersec <128181057@sastra.ac.in>
2026-04-09 15:18:51 +0530

    Create __init__.py

---
+++ server/__init__.py
@@ -0,0 +1,5 @@
+"""
+Server package initialization.
+This file ensures the server directory is treated as a Python package.
+No heavy imports should be placed here to avoid circular dependencies.
+"""
